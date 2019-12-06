25d2a2c619467ca0888dce79a86eeea9d207733e
ethrogmorton <57643915+ethrogmorton@users.noreply.github.com>
2019-12-06 17:13:57 -0500

    Completed module

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+let myQueue = [];
+myQueue.push(1);
+myQueue.push(2);
+myQueue.push(3);
+
+console.log(myQueue.shift());
+console.log(myQueue.shift());
+console.log(myQueue.shift());
--- js/ratings.js
+++ js/ratings.js
@@ -21,3 +21,21 @@ document.addEventListener('change', () => {
     const ratings = collect_ratings();
     document.querySelector('#average').value = ratings.average.toFixed(2);
 });
+
+var myQueue = [];
+myQueue.push(1);
+myQueue.push(2);
+myQueue.push(3);
+myQueue.push(4);
+myQueue.push(5);
+myQueue.push(6);
+myQueue.push(7);
+myQueue.push(8);
+myQueue.push(9);
+
+
+
+myQueue.forEach(element => {
+    console.log(element)
+
+});
